20ecf296c155ce7b03daf55408e4dbe6d42050b5
Owen Roth <rothowen27@gmail.com>
2023-02-10 08:27:36 -0600

    refactor client methods to make them more lightweight

--- lib/easy_hubspot/client.rb
+++ lib/easy_hubspot/client.rb
@@ -4,44 +4,28 @@ module EasyHubspot
   # EasyHubspot::Client
   class Client
     class << self
-      def do_get(path = nil, headers = {}, format_plain: true)
-        if format_plain
-          response = HTTParty.get("#{EasyHubspot.configuration.base_url}#{path}", headers: headers,
+      def do_get(path = nil, headers = {})
+        response = HTTParty.get("#{EasyHubspot.configuration.base_url}#{path}", headers: headers,
+                                                                                format: :plain)
+        parse_response(response)
+      end
+
+      def do_post(path = nil, body = {}, headers = {})
+        response = HTTParty.post("#{EasyHubspot.configuration.base_url}#{path}", body: body, headers: headers,
+                                                                                 format: :plain)
+        parse_response(response)
+      end
+
+      def do_patch(path = nil, body = {}, headers = {})
+        response = HTTParty.patch("#{EasyHubspot.configuration.base_url}#{path}", body: body, headers: headers,
                                                                                   format: :plain)
-          parse_response(response)
-        else
-          HTTParty.get("#{EasyHubspot.configuration.base_url}#{path}", headers: headers)
-        end
+        parse_response(response)
       end
 
-      def do_post(path = nil, body = {}, headers = {}, format_plain: true)
-        if format_plain
-          response = HTTParty.post("#{EasyHubspot.configuration.base_url}#{path}", body: body, headers: headers,
+      def do_delete(path = nil, headers = {})
+        response = HTTParty.delete("#{EasyHubspot.configuration.base_url}#{path}", headers: headers,
                                                                                    format: :plain)
-          parse_response(response)
-        else
-          HTTParty.post("#{EasyHubspot.configuration.base_url}#{path}", body: body, headers: headers)
-        end
-      end
-
-      def do_patch(path = nil, body = {}, headers = {}, format_plain: true)
-        if format_plain
-          response = HTTParty.patch("#{EasyHubspot.configuration.base_url}#{path}", body: body, headers: headers,
-                                                                                    format: :plain)
-          parse_response(response)
-        else
-          HTTParty.patch("#{EasyHubspot.configuration.base_url}#{path}", body: body, headers: headers)
-        end
-      end
-
-      def do_delete(path = nil, headers = {}, format_plain: true)
-        if format_plain
-          response = HTTParty.delete("#{EasyHubspot.configuration.base_url}#{path}", headers: headers,
-                                                                                     format: :plain)
-          parse_response(response)
-        else
-          HTTParty.delete("#{EasyHubspot.configuration.base_url}#{path}", headers: headers)
-        end
+        parse_response(response)
       end
 
       private
